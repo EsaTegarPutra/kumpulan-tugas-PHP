14a81b72e5b1b3216db8da84edd3d12ab110d466
EsaTegarPutra <139981385+EsaTegarPutra@users.noreply.github.com>
2023-07-31 07:32:49 +0700

    Add files via upload

---
+++ php-oop/oop.php
@@ -0,0 +1,93 @@
+<?php
+    class Pakaian{
+        public $jenis;
+        private $harga;
+        protected $ukuran;
+        
+        function __construct($harga,$ukuran){
+            $this->harga = $harga;
+            $this->ukuran = $ukuran;
+        }
+
+        function __destruct(){
+            echo "{$this->jenis} dengan ukuran {$this->ukuran} sangat nyaman dipakai";
+        }
+
+        function setPakaian($jenis){
+            $this->jenis = $jenis;
+        }
+
+        function getPakaian(){
+            return "Pakaian telah disediakan";
+        }
+
+        function describe(){
+            echo ("<br>Jenis: {$this->jenis}<br>Harga: {$this->harga}<br>ukuran: {$this-> ukuran}<br>");
+        }
+   
+    }
+
+    class Baju extends Pakaian{
+        private $merk;
+
+        function __construct($harga,$ukuran){
+            parent:: __construct($harga,$ukuran);
+        }
+
+        function __destruct(){
+            echo "Baju {$this->merk} dengan ukuran {$this->ukuran} sangat nyaman dipakai.<br>";
+        }
+
+        function setBaju($jenis, $merk){
+            $this->jenis = $jenis;
+            $this->merk = $merk;
+        }
+
+        function getBaju(){
+            return "Baju Telah disediakan";
+        }
+    }
+    
+    class Sepatu extends Pakaian{
+        private $merk;
+
+        function __construct($harga,$ukuran){
+            parent:: __construct($harga,$ukuran);
+        }
+
+        function __destruct(){
+            echo "Sepatu {$this->merk} dengan ukuran {$this->ukuran} sangat nyaman dipakai.<br>";
+        }
+
+        function setSepatu($jenis, $merk){
+            $this->jenis = $jenis;
+            $this->merk = $merk;
+        }
+
+        function getSepatu(){
+            return "Sepatu Telah disediakan";
+        }
+    }
+    
+    
+    $pakaian = new Pakaian(100000,"XL");    
+    $pakaian->setPakaian("Pakaian Seragam");
+    echo "{$pakaian->getPakaian()}";
+    $pakaian->describe();
+
+    echo"<br>";
+
+    $baju = new Baju (200000,"L ");
+    $baju->setBaju("Baju Sport","Polo");
+    echo "{$baju->getBaju()}";
+    $baju->describe();
+
+    echo "<br>";
+
+    $Sepatu = new Sepatu (300000,"42");
+    $Sepatu->setSepatu("Sepatu Sport","Nike");
+    echo "{$Sepatu->getSepatu()}";
+    $Sepatu->describe();
+
+    echo "<br>";
+?>
